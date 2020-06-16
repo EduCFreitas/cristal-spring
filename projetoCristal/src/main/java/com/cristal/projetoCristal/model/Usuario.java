f1fef55d8f5a1855d2daf9a5cdd3cdd4bbab4156
José Eduardo Candido de Freitas <jeduardocfreitas@gmail.com>
2020-06-16 17:03:47 -0300

    Solucionado problema de getters e setters

--- projetoCristal/src/main/java/com/cristal/projetoCristal/model/Usuario.java
+++ projetoCristal/src/main/java/com/cristal/projetoCristal/model/Usuario.java
@@ -106,8 +106,14 @@ public class Usuario {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
 	public String getEmail() {
 		return email;
